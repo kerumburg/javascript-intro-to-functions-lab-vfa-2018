83c65e761c0ed02aa4abcf30b414fcb57900b099
Kelsey Rumburg <kerumburg@gmail.com>
2018-06-20 14:47:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
-  console.log(shout('hello'))
+  console.log(string.toUpperCase())
 }
- function logWhisper(string){
-   console.log(whisper('HELLO'))
- }
- function sayHitoGrandma(string){
-  if(string.toLowerCase()===string){
-   return "I can\'t hear you!";
- }
-  if(string.toUpperCase()===string){
-   return "YES INDEED!";
- }
-  if(string==="I love you, grandma."){
-   return "I love you, too.";
- }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
